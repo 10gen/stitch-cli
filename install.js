269d61ff8ee0c1eb2cba6c91c8f78055e634cf4f
Hai-Kinh Hoang <haikinh.hoang@10gen.com>
2018-06-26 12:10:27 -0400

    STITCH-1456 - add npm install script

---
+++ install.js
@@ -0,0 +1,98 @@
+const fs = require('fs');
+const path = require('path');
+const request = require('request');
+
+const baseDownloadURL =
+  'https://s3.amazonaws.com/stitch-clis/stitch_cli_linux_64_69ab0a733f209eda9901c1aea8a19e5c2e5fd862_18_06_05_16_56_25';
+const linuxDownloadURL = `${baseDownloadURL}/linux-amd64/stitch-cli`;
+const macDownloadURL = `${baseDownloadURL}/macos-amd64/stitch-cli`;
+const windowsDownloadURL = `${baseDownloadURL}/windows-amd64/stitch-cli.exe`;
+
+function getDownloadURL() {
+  const platform = process.platform;
+  let downloadUrl;
+
+  if (platform === 'linux') {
+    if (process.arch === 'x64') {
+      downloadUrl = linuxDownloadURL;
+    } else {
+      return Promise.reject(new Error('Only Linux 64 bits supported.'));
+    }
+  } else if (platform === 'darwin' || platform === 'freebsd') {
+    if (process.arch === 'x64') {
+      downloadUrl = macDownloadURL;
+    } else {
+      return Promise.reject(new Error('Only Mac 64 bits supported.'));
+    }
+  } else if (platform === 'win32') {
+    if (process.arch === 'x64') {
+      downloadUrl = windowsDownloadURL;
+    } else {
+      return Promise.reject(new Error('Only Windows 64 bits supported.'));
+    }
+  } else {
+    return Promise.reject(new Error(`Unexpected platform or architecture: ${process.platform} ${process.arch}`));
+  }
+
+  return Promise.resolve(downloadUrl);
+}
+
+function fixFilePermissions(filePath) {
+  // Check that the binary is user-executable and fix it if it isn't
+  if (process.platform !== 'win32') {
+    const stat = fs.statSync(filePath);
+    // 64 == 0100 (no octal literal in strict mode)
+    // eslint-disable-next-line no-bitwise
+    if (!(stat.mode & 64)) {
+      console.log('Fixing file permissions');
+      fs.chmodSync(filePath, '755');
+    }
+  }
+}
+
+function requstBinary(downloadUrl) {
+  console.log(`downloading stitch-cli from "${downloadUrl}"`);
+
+  return new Promise((resolve, reject) => {
+    let count = 0;
+    let notifiedCount = 0;
+
+    const binaryName = process.platform === 'win32' ? 'stitch-cli.exe' : 'stitch-cli';
+    const filePath = path.join(process.cwd(), binaryName);
+    const outFile = fs.openSync(filePath, 'w');
+
+    const requestOptions = {
+      uri: downloadUrl,
+      method: 'GET',
+    };
+    const client = request(requestOptions);
+
+    client.on('error', err => {
+      reject(new Error(`Error with http(s) request: ${err}`));
+    });
+
+    client.on('data', data => {
+      fs.writeSync(outFile, data, 0, data.length, null);
+      count += data.length;
+      if (count - notifiedCount > 800000) {
+        process.stdout.write(`Received ${Math.floor(count / 1024)} K...\r`);
+        notifiedCount = count;
+      }
+    });
+
+    client.on('end', () => {
+      console.log(`Received ${Math.floor(count / 1024)} K total.`);
+      fs.closeSync(outFile);
+      fixFilePermissions(filePath);
+      resolve(true);
+    });
+  });
+}
+
+// Install script starts here
+getDownloadURL()
+  .then(downloadUrl => requstBinary(downloadUrl))
+  .catch(err => {
+    console.error('Stitch CLI installation failed:', err);
+    process.exit(1);
+  });
