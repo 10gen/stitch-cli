22e4d388b7975d45f55a2d765ad9e32acc254ee3
Julia Ruddy <jruddy815@gmail.com>
2019-10-18 14:34:11 -0400

    remove comment

--- hosting/models.go
+++ hosting/models.go
@@ -43,7 +43,6 @@ type AssetMetadata struct {
 
 // IsDir is true if the asset represents a directory
 func (amd *AssetMetadata) IsDir() bool {
-	// return strings.HasSuffix(amd.FilePath, "\\")
 	return strings.HasSuffix(amd.FilePath, string(os.PathSeparator))
 }
 
